ba80f0b5dd79ff394ad71dbedac87a8c07b7e7a5
khalil <khalil@r2k.co.il>
2023-09-22 08:57:02 +0300

    fix bugs

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,16 @@ module.exports = function(grunt){
     grunt.initConfig({
         //pass in options to plugins, refrences to files etc
         concat : {
-            docs : {
+         /*   docs : {
                 src : [ 'src/index.html'],
                 dest : 'dist/index.html'
-            },
+            },*/
             js : {
-                src : [ 'src/js/*.js'],
+                src : [ 'src/js/light-table.js'],
                 dest : 'dist/js/light-table.js'
             },
             css : {
-                src : [ 'src/css/*.css'],
+                src : [ 'src/css/style.css'],
                 dest : 'dist/css/style.css'
             }
         },
@@ -23,16 +23,35 @@ module.exports = function(grunt){
                     {
                         src :  'dist/js/light-table.js',
                         dest : 'dist/js/light-table.js',
-                    },
-                    /* 
-                    {
-                        src :  'dist/style.css',
-                        dest : 'dist/style.css',
-                    },
-                    */
+                    }
                 ]
             } 
         },
+        cssmin: {
+            options: {
+              mergeIntoShorthands: false,
+              roundingPrecision: -1
+            },
+            target: {
+              files: {
+                'dist/css/style.css' : ['dist/css/style.css']
+              }
+            }
+          },
+          copy: {
+            main: {
+              src: 'src/css/demo.css',
+              dest: 'dist/css/demo.css',
+            },
+            demo: {
+
+                expand: true,
+                cwd: 'src/',
+                filter: 'isFile',
+              src: '**.html',
+              dest: 'dist/',
+            },
+          },
         clean: {
             dist: [ 'dist' ]
         },
@@ -43,16 +62,15 @@ module.exports = function(grunt){
     //load plugins
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
  
 
-    // register tasks
-    //grunt.registerTask('concat-js',['concat:js']);
-    
 
-    grunt.registerTask('dist',[ 'clean:dist', 'concat','uglify']);
+    grunt.registerTask('dist',[ 'clean:dist', 'concat','uglify','cssmin','copy','copy:demo']);
 
   //  grunt.registerTask('serve', [ 'connect:server', 'watch' ]);
 
