b86b21b3c0a272ae0a21836b760d0d15463becf0
khalil <khalil@r2k.co.il>
2023-09-25 05:54:42 +0300

    Charts bars

--- src/js/light-table.js
+++ src/js/light-table.js
@@ -1,25 +1,31 @@
 class LightTable {
+ 
 
-   
     constructor(options) {
-
+ 
+ 
         //default options
         const defaults = { 
             selector : 'table',  // string
             stickyHead : false, // boolean true | false
             search : false, // boolean true | false
             
+            //pagination
             pagination :false,
             paginationPerPage :10,
+
+            //theme
             theme : 'black', //string
 
-
+            //export options
             print :false,
             exportCSV :false,
             exportCSVFileName :'',
 
+            //charts
+            charts :[],//charts 
 
-
+            //strings
             labels : {
                 print : 'Print',
                 exportCSV : 'Export CSV',
@@ -42,7 +48,7 @@ class LightTable {
         }
 
          //draw tables
-       
+     
         elements.forEach(element => {
           
             let tableIndex=(Math.floor(Math.random() * 1000) + 1 ) + (Math.floor(Math.random() * 100) + 1 )
@@ -80,6 +86,11 @@ class LightTable {
             let footer = document.createElement('div');
             footer.classList.add("lt-table-footer");
             wrapper.appendChild(footer);
+ 
+            //add charts
+            let charts = document.createElement('div');
+            charts.classList.add("lt-table-charts");
+            wrapper.appendChild(charts);
 
 
             //remove the org table 
@@ -163,9 +174,24 @@ class LightTable {
                 });
             });
 
+
+
+            if(options.charts.length>0){
+               
+                options.charts.forEach((chart) => {  
+                    LightTable.#loadChart(wrapper,chart); 
+                });
+              
+            }
+     
+
              
         });
- 
+
+
+
+
+   
 
     
 
@@ -526,5 +552,249 @@ class LightTable {
         tartgetTbody.innerHTML=html;
     }
  
+
+
+    static #loadChart(p,chart){
+
+        let magic=Math.floor(Math.random() * 1000) + 1;
+        const svgId = 'lt-table-charts-'+magic;
+        const chartWrapper = p.querySelectorAll('.lt-table-charts')[0];
+
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('id', svgId);
+        chartWrapper.appendChild(svg);
+
+
+        const args = [p,chart,svgId];
+//index
+
+
+
+        //load bars
+        LightTable.#loadChartBars(args); 
+
+ 
+
+      window.addEventListener('resize',function(){
+        LightTable.#loadChartBars(args);
+      });
+
+    }
+    static #loadChartBars(args){
+
+        let p = args[0];
+        let chart = args[1];
+        let svgId = args[2];
+ 
+ 
+        const svg = document.getElementById(svgId);
+       // const trs =  p.querySelectorAll('tbody tr');
+
+        //source
+        let source = document.querySelectorAll('table[data-id="'+p.dataset.id+'"]')[0];
+        let trs =  source.querySelectorAll('tbody tr');
+    
+         
+
+        svg.innerHTML='';
+
+        let data = [];
+        trs.forEach((tr,index) => {
+         
+            console.log(tr.querySelectorAll('td')[chart.x]);
+            if(typeof tr.querySelectorAll('td')[chart.x]!='undefined'
+            && typeof tr.querySelectorAll('td')[chart.y]!='undefined'){
+                const xVal = tr.querySelectorAll('td')[chart.x].textContent;
+                const   yVal = tr.querySelectorAll('td')[chart.y].textContent;
+                const ob = {
+                    label : xVal,
+                    value : yVal
+                };
+                data.push(ob);  
+            }
+        });
+        if(data.length==0){
+            console.warn('data not found in table!');
+            return;
+        }
+ 
+
+
+   
+        //svg.clientWidth
+
+        let dataValues = data.map(function (item) {
+            return item.value;
+        });
+
+
+        const maxValue = Math.max(...dataValues);
+    
+        // Calculate the coordinates and dimensions
+        let width =  !chart.hideAxis ? svg.clientWidth - 60 : svg.clientWidth;
+        let height =  !chart.hideAxis ? svg.clientHeight - 60 : svg.clientHeight;
+
+        let xScale = (width - chart.barSpacing * (data.length - 1)) / data.length;
+        let yScale = height / maxValue;
+ 
+        //horizintal lines
+        console.log(chart.showHorizintalLines);
+        if(chart.showHorizintalLines){
+            // Add labels to y-axis
+            for (let i = 1; i <= maxValue; i += Math.round(maxValue / chart.axisScaleUnit)) {
+                const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+                line.setAttribute('transform', 'translate(30 30)');
+                line.setAttribute('x1', 0);
+                line.setAttribute('y1',height - i * yScale);
+                line.setAttribute('x2', width);
+                line.setAttribute('y2', height - i * yScale);
+                line.setAttribute('stroke', chart.horizintalLinesColors);
+                line.setAttribute('stroke-width', '1');
+                svg.appendChild(line);
+            }
+        }
+
+
+
+        //visualization group
+        const vGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        vGroup.classList.add("lt-chart-visual");
+        if(!chart.hideAxis){
+            vGroup.setAttribute('transform', 'translate(30 30)');
+        }else{
+            vGroup.setAttribute('transform', 'translate(0 0)');
+        }
+         
+        vGroup.setAttribute('width', width);
+        vGroup.setAttribute('height', height);
+    
+        svg.appendChild(vGroup);
+
+
+
+        
+        // Draw the bars and labels
+        for (let i = 0; i < data.length; i++) {
+
+            // Create a group for slices and labels
+            const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+            group.classList.add("lt-chart-item");
+            vGroup.appendChild(group);
+
+
+            
+            const value = data[i].value;
+            const barHeight = value * yScale;
+         
+            const barWidth = xScale;
+            const x = i * (barWidth + chart.barSpacing);
+            const y = height - barHeight;
+
+ 
+            // Draw the bar
+            const bar = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            bar.setAttribute('x', x);
+            bar.setAttribute('y', y);
+            bar.setAttribute('width', barWidth);
+            bar.setAttribute('height', barHeight);
+            bar.setAttribute('fill', chart.color);
+ 
+
+            group.appendChild(bar);
+
+
+              // hoverText
+              if(chart.hover){
+                const label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+                label.classList.add("lt-chart-hover");
+                label.setAttribute('x', x + barWidth / 2);
+                label.setAttribute('y', y + barHeight / 2);
+                 
+                  label.setAttribute('fill', chart.hoverColor);
+              
+                label.setAttribute('text-anchor', 'middle');
+                label.textContent =value.toString();
+                group.appendChild(label);
+              }
+      
+       
+            // Add label
+            if(!chart.barHideLabel){
+              const label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+              label.setAttribute('x', x + barWidth / 2);
+              label.setAttribute('y', y - 5);
+              label.setAttribute('text-anchor', 'middle');
+              label.textContent = value.toString();
+              group.appendChild(label);
+            }
+        }
+
+
+
+        
+        if(!chart.hideAxis){
+   
+  
+            const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+            group.classList.add("lt-chart-axis");
+            group.setAttribute('transform', 'translate(30 30)');
+            svg.appendChild(group);
+  
+            const barWidth = xScale;
+  
+              // Draw the x-axis
+              const xAxis = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+              xAxis.setAttribute('x1', 0);
+              xAxis.setAttribute('y1', height);
+              xAxis.setAttribute('x2', width);
+              xAxis.setAttribute('y2', height);
+              xAxis.setAttribute('stroke', '#787878');
+              xAxis.setAttribute('stroke-width', '1');
+              group.appendChild(xAxis);
+          
+              // Draw the y-axis
+              const yAxis = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+              yAxis.setAttribute('x1', 0);
+              yAxis.setAttribute('y1', 0);
+              yAxis.setAttribute('x2', 0);
+              yAxis.setAttribute('y2', height);
+              yAxis.setAttribute('stroke', '#787878');
+              yAxis.setAttribute('stroke-width', '1');
+              group.appendChild(yAxis);
+          
+              // Add labels to x-axis
+  
+              for (let i = 0; i < data.length; i++) {
+  
+  
+                const label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+                label.setAttribute('x', i * (barWidth + chart.barSpacing) + barWidth / 2);
+                label.setAttribute('y', height + 15);
+                label.setAttribute('text-anchor', 'middle');
+                label.textContent = data[i].label;
+                label.setAttribute('fill', '#787878');
+                group.appendChild(label);
+              }
+          
+              // Add labels to y-axis
+              for (let i = 0; i <= maxValue; i += Math.round(maxValue / chart.axisScaleUnit)) {
+                const label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+                label.setAttribute('x', -5);
+                label.setAttribute('y', height - i * yScale);
+                label.setAttribute('text-anchor', 'end');
+                label.setAttribute('alignment-baseline', 'middle');
+                label.textContent = i.toString();
+                label.setAttribute('fill', '#787878');
+                group.appendChild(label);
+              }
+          }
+        
+ 
+
+     
+
+
+//lt-table-charts
+    }
 
 }
